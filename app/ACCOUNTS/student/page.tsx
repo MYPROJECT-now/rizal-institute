94a464d400bd1d3a948084aea1c14b43129f1068
Ian Red Clarenz E. Divinagracia <ianreddivinagracia81@gmail.com>
2025-09-15 02:51:50 +0800

    mobile

--- app/ACCOUNTS/student/page.tsx
+++ app/ACCOUNTS/student/page.tsx
@@ -3,7 +3,7 @@ import { StudentDashboard } from "@/components/accounts/students/dashboard/dashb
 
 const Dashboard = () => {
     return (
-            <div className="w-full sm:max-w-full sm:px-8 px-0  max-w-[300px] h-[680px] sm:mt-3 mt-0 mx-3 mb-3 rounded-xl flex flex-col  bg-page">
+            <div className="w-full sm:max-w-full sm:px-8 px-0  min-w-[300px] h-[680px] sm:mt-3 mt-0 mx-3 mb-3 rounded-xl flex flex-col  bg-page">
                 <Admin_student />
                 <div className="w-full h-auto lg:h-[540px] bg-white self-center mt-2 lg:mt-10 rounded-lg shadow-lg overflow-hidden border border-gray-200">
                     <div className="h-full flex flex-col">
--- app/ACCOUNTS/student/payments/page.tsx
+++ app/ACCOUNTS/student/payments/page.tsx
@@ -5,7 +5,7 @@ import { PaymentPage } from "./paymentPage";
 
 const Payments = () => {
     return (
-            <div className="w-full sm:max-w-full sm:px-8 px-2  max-w-[300px] min-h-[680px] mt-3 mx-3 mb-3 rounded-xl flex flex-col  bg-page">
+            <div className="w-full sm:max-w-full sm:px-8 px-2  min-w-[300px] min-h-[680px] mt-3 mx-3 mb-3 rounded-xl flex flex-col  bg-page">
             <Admin_student />
             <div className="w-full h-auto lg:h-[540px] bg-white self-center  mt-10 rounded-lg ">
                 <div className="sm:h-[80px] h-[60px] w-full bg-lGreen font-merriweather text-md sm:text-xl lg::text-3xl text-white items-center flex pl-5">
